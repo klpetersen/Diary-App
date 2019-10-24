734a8124e2ee63186d698c8020ac129d04e61727
github username <github email address>
2019-10-24 15:26:17 -0400

    mvc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user1 = User.create(name: 'Yoan', username: 'cuban-cabana-boy')
+user2 = User.create(name: 'Tony', username: 'xXRedDarknessXx')
+user3 = User.create(name: 'Seb', username: 'MurderPunch')
+
+
+
+post1 = Post.create(title: 'First post', content: 'This is content of first post', user_id: 1)
