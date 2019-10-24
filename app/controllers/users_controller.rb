682f933bc4af7762e3cb6263fa25ceedded64331
github username <github email address>
2019-10-24 18:49:21 -0400

    login complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
         users = User.all 
         render json: UserSerializer.new(users).serialized_json 
     end 
+    
+    def create
+        data = JSON.parse(request.raw_post)
+        user = User.create(data)
+    end
 end
